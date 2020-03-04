ba211b6dfad29a6a82b8bcc3c7edaf2e8b53deb2
Patricica Kaluza <petulacicle@gmail.com>
2020-03-04 17:19:20 +0100

    python.py aktualisiert

--- python.py
+++ python.py
@@ -3,3 +3,5 @@ print("How are you doing?")
 Das soll jetzt im Design Branch stattfinden
 jetzt wird wieder was geändert
 hahahahah
+
+Jo hallo hier wieder in Design jetzt sind wir fertig wir können mit search mergen

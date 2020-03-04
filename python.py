7f90e16de4fc0490c1ede0e165f08285664a7de3
Patricica Kaluza <petulacicle@gmail.com>
2020-03-04 17:15:00 +0100

    python.py aktualisiert

--- python.py
+++ python.py
@@ -1,9 +1,9 @@
 print("Hello World")
 print("How are you doing?")
-<<<<<<< HEAD
+
 jetzt im master
-=======
+
 Das soll jetzt im Design Branch stattfinden
 jetzt wird wieder was geändert
 hahahahah
->>>>>>> Design
+

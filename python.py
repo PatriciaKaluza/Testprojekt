1cccdf421020eddd714b1dd4ae1f437f8e7a66be
Patricica Kaluza <petulacicle@gmail.com>
2020-03-04 17:16:09 +0100

    python.py aktualisiert

--- python.py
+++ python.py
@@ -1,9 +1,1 @@
-print("Hello World")
-print("How are you doing?")
-
-jetzt im master
-
-Das soll jetzt im Design Branch stattfinden
-jetzt wird wieder was geändert
-hahahahah
-
+Jetzt ist das der Search Branch
